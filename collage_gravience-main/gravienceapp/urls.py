b9eaeb688b1edf6890e5b0d7b8b2f4c573e6c4ce
abaththa15 <157196638+abaththa15@users.noreply.github.com>
2025-08-16 12:07:49 +0530

    Add files via upload

---
+++ collage_gravience-main/gravienceapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path # type: ignore
+from . import views
+
+urlpatterns = [
+    path('viewdata/', views.viewdata),
+    path('student-grievance/', views.grievance_form, name="student_grievance"),
+]
