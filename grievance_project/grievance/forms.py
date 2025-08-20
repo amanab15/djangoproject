8e4cbc8f9e28f6366a0e969379fca78459566608
abaththa15 <157196638+abaththa15@users.noreply.github.com>
2025-08-20 18:41:03 +0530

    Delete grievance_project directory

--- grievance_project/grievance/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Grievance
-
-class GrievanceForm(forms.ModelForm):
-    class Meta:
-        model = Grievance
-        fields = ['title', 'description', 'status']
--- grievance_project/grievance/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.grievance_list, name='grievance_list'),
-    path('create/', views.grievance_create, name='grievance_create'),
-    path('update/<int:pk>/', views.grievance_update, name='grievance_update'),
-    path('delete/<int:pk>/', views.grievance_delete, name='grievance_delete'),
-]
