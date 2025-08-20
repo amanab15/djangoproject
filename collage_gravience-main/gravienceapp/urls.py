df48a739c5e6cf1c444640942a5527e4e9f77e59
abaththa15 <157196638+abaththa15@users.noreply.github.com>
2025-08-20 18:12:37 +0530

    Delete collage_gravience-main directory

--- collage_gravience-main/gravienceapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path # type: ignore
-from . import views
-
-urlpatterns = [
-    path('viewdata/', views.viewdata),
-    path('student-grievance/', views.grievance_form, name="student_grievance"),
-]
