80c54246b5bc972606536ad3c4f53a51d30087b1
Aman Baththa <157196638+amanab15@users.noreply.github.com>
2025-08-21 11:40:51 +0530

    Delete collage_gravience-main directory

--- collage_gravience-main/gravienceapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Grievance
-
-@admin.register(Grievance)
-class GrievanceAdmin(admin.ModelAdmin):
-    list_display = ("id", "student_name", "email", "subject", "description", "created_at")
--- collage_gravience-main/gravienceapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path # type: ignore
-from . import views
-
-urlpatterns = [
-    path('viewdata/', views.viewdata),
-    path('admin/', views.viewdata),
-    path('student-grievance/', views.grievance_form, name="student_grievance"),
-]
