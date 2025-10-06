ba45012117333eef532cd82f432f03c04fe1e519
Aman Baththa <157196638+amanab15@users.noreply.github.com>
2025-10-06 11:09:05 +0530

    Delete collage_gravience-main directory

--- collage_gravience-main/gravienceapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Grievance
-
-@admin.register(Grievance)
-class GrievanceAdmin(admin.ModelAdmin):
-    list_display = ('student_name', 'email', 'subject', 'created_at')
-    list_filter = ('created_at',)
-    search_fields = ('student_name', 'email', 'subject', 'description')
